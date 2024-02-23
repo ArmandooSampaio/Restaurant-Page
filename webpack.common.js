07386f54aa60cb5fc7dfc147a2a10779285cc5bc
Armando Sampaio <armandoosampai0@gmail.com>
2024-02-23 15:41:54 -0300

    Configurando Webpack

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+
+module.exports = {
+    entry: {
+        filename: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        assetModuleFilename: './image/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject:'body'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
+
+
+
+}
+
